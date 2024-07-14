fc2d09011341f9ce9a57a6096f1b63a10d699dce
alone <154563439+neverfriendme@users.noreply.github.com>
2024-07-13 21:49:06 -0400

    Update all_equal.py

--- all_equal.py
+++ all_equal.py
@@ -1,8 +1,8 @@
 def all_equal(arg1,arg2,arg3):
     if arg1 == arg2 == arg3:
         if True:
-            print("True")
+            return True
     else:
-        print("False")
+        return False
 
 all_equal(1,1,1)
